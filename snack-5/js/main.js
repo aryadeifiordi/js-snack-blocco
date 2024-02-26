7c8f57cd7f08ec935e9d5e0896a99fecd4ba3307
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-26 12:04:36 +0100

    done

--- snack-5/js/main.js
+++ snack-5/js/main.js
@@ -1,1 +1,18 @@
-console.log("work")
+//console.log("work")
+
+var numeriDispari = []
+
+// Ciclo per chiedere all'utente di inserire un numero per 6 volte
+for (var i = 0; i < 6; i++) {
+    var numero = parseInt(prompt("Inserisci un numero:"));
+    
+    if (numero % 2 !== 0) {
+        
+        numeriDispari.push(numero);
+    }
+}
+
+console.log("Numeri dispari inseriti:", numeriDispari);
+
+
+
