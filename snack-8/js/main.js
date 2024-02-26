3e26ac5e149355a2393868e8049ffb8da00e2535
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-26 17:02:48 +0100

    snack-pomeriggio

--- snack-8/js/main.js
+++ snack-8/js/main.js
@@ -1,6 +1,19 @@
 //console.log("work");
 
 
-let numero = prompt("Inserisci un numero di 4 cifre:");
+let somma = 0;
 
+let userNumber = prompt("inserisci un numero di 4 cifre:")
 
+for (let 0index = 0; index < userNumber.length; index++) {
+    let element = userNumber[index];
+    element = Number(element);
+    somma += element,
+
+}
+
+console.log(somma);
+
+userNumber = Number(userNumber);
+
+console.log(userNumber);
